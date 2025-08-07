f7306217cd9155a0630498956f70c9de7eaa4809
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-08-07 18:34:49 +0930

    updated bg color

--- src/components/Todobox.jsx
+++ src/components/Todobox.jsx
@@ -2,7 +2,7 @@
         return(
             <>
             {todos.map((todo)=>(
-                <div key={todo.id} className="h-11 bg-pink-200 rounded-md px-5 flex items-center font-semibold">{todo.todo}</div>
+                <div key={todo.id} className="h-11 bg-blue-100 rounded-md px-5 flex items-center font-semibold">{todo.todo}</div>
                 
             ))}
             </>
