8a62b2625ec62b7f34602e38e0b00a9782d8b9c6
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-08-07 18:36:03 +0930

    integrated tobobox component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Todoform } from './components/Todoform'
+import { useTodo } from './contexts/Todocontext'
+import { Todobox } from './components/Todobox'
 
 function App() {
   const [count, setCount] = useState(0)
+  const { todos } = useTodo()
 
   return (
     <>
-      <div className='text-red-400'>hello</div>
+
+      <div className='font-bold text-white text-2xl text-center py-8'>Manage Your Todos</div>
+      <div className='w-[70vw] m-auto flex flex-col gap-4 '>
+        <Todoform />
+
+       
+        <Todobox todos={todos}/>
+      </div>
+
+
     </>
   )
 }
