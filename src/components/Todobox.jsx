e12a1ec75046fef686d2fbbd5fd04deb08d2f1e2
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-08-07 19:09:28 +0930

    todobox toggle color as checked

--- src/components/Todobox.jsx
+++ src/components/Todobox.jsx
@@ -1,11 +1,20 @@
-    export const Todobox=({todos})=>{
-        return(
-            <>
-            {todos.map((todo)=>(
-                <div key={todo.id} className="h-11 bg-blue-100 rounded-md px-5 flex items-center font-semibold">{todo.todo}</div>
-                
+import { useTodo } from "../contexts/Todocontext"
+export const Todobox = ({ todos }) => {
+    const {toggletodo}=useTodo()
+
+    return (
+        <>
+            {todos.map((todo) => (
+                <div key={todo.id} className={`h-11 ${todo.completed?'bg-green-200':'bg-blue-100'} rounded-md px-5 flex items-center font-semibold`}>
+                    <div className="flex gap-2 ">
+
+                    <input type="checkbox" onChange={()=>toggletodo(todo.id)} checked={todo.completed}/>
+                    {todo.todo}
+                    </div>
+                </div>
+
             ))}
-            </>
-            
-        )   
-    }
+        </>
+
+    )
+}
